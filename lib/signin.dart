59b2448d485db714761fa87b1fc0604ac334178a
a <a>
2025-04-19 18:12:18 +0300

    Add changes

--- lib/signin.dart
+++ lib/signin.dart
@@ -3,15 +3,22 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'password/request_reset_password.dart';
 import 'describe_feeling.dart';
+class SignInPage extends StatefulWidget {
+  const SignInPage({Key? key}) : super(key: key);
 
-class SignInPage extends StatelessWidget {
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
   final TextEditingController usernameOrEmail = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-  final String baseUrl = "https://anees-rus4.onrender.com"; 
+  final String baseUrl = "https://anees-rus4.onrender.com";
+
+  bool isPasswordVisible = false;
 
   Future<void> signInRequest(BuildContext context) async {
     final Uri url = Uri.parse("$baseUrl/signin");
-
     final username = usernameOrEmail.text.trim();
     final password = passwordController.text.trim();
 
@@ -22,10 +29,7 @@ class SignInPage extends StatelessWidget {
       return;
     }
 
-    final Map<String, dynamic> requestData = {
-      "username": username,
-      "password": password,
-    };
+    final requestData = {"username": username, "password": password};
 
     try {
       final response = await http.post(
@@ -79,7 +83,7 @@ class SignInPage extends StatelessWidget {
                       context,
                       MaterialPageRoute(builder: (context) => const RequestResetPasswordPage()),
                     );
-                  }, 
+                  },
                   child: const Text(
                     "نسيت كلمة المرور",
                     style: TextStyle(
@@ -104,56 +108,51 @@ class SignInPage extends StatelessWidget {
   }
 
   Widget buildTextField(String label, String hint, bool isPassword, TextEditingController controller) {
-    return StatefulBuilder(
-      builder: (BuildContext context, StateSetter setState) {
-        bool isObscured = isPassword;
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Text(
-              label,
-              style: const TextStyle(
-                color: Color(0xFF4F6DA3),
-                fontWeight: FontWeight.bold,
-                fontSize: 17,
-                fontFamily: 'Tienne',
-                letterSpacing: -0.16,
-              ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Text(
+          label,
+          style: const TextStyle(
+            color: Color(0xFF4F6DA3),
+            fontWeight: FontWeight.bold,
+            fontSize: 17,
+            fontFamily: 'Tienne',
+            letterSpacing: -0.16,
+          ),
+        ),
+        const SizedBox(height: 8),
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          decoration: BoxDecoration(
+            color: const Color(0xFFE9F1F4),
+            borderRadius: BorderRadius.circular(32),
+          ),
+          child: TextField(
+            controller: controller,
+            obscureText: isPassword ? !isPasswordVisible : false,
+            textAlign: TextAlign.right,
+            decoration: InputDecoration(
+              hintText: hint,
+              hintStyle: TextStyle(color: Colors.black.withOpacity(0.4)),
+              border: InputBorder.none,
+              prefixIcon: isPassword
+                  ? IconButton(
+                      icon: Icon(
+                        isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                        color: Colors.grey,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          isPasswordVisible = !isPasswordVisible;
+                        });
+                      },
+                    )
+                  : null,
             ),
-            const SizedBox(height: 8),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              decoration: BoxDecoration(
-                color: const Color(0xFFE9F1F4),
-                borderRadius: BorderRadius.circular(32),
-              ),
-              child: TextField(
-                controller: controller,
-                obscureText: isPassword && isObscured,
-                textAlign: TextAlign.right,
-                decoration: InputDecoration(
-                  hintText: hint,
-                  hintStyle: TextStyle(color: Colors.black.withOpacity(0.4)),
-                  border: InputBorder.none,
-                  prefixIcon: isPassword 
-                      ? IconButton(
-                          icon: Icon(
-                            isObscured ? Icons.visibility_off : Icons.visibility,
-                            color: Colors.grey,
-                          ),
-                          onPressed: () {
-                            setState(() {
-                              isObscured = !isObscured;
-                            });
-                          },
-                        )
-                      : null,
-                ),
-              ),
-            ),
-          ],
-        );
-      },
+          ),
+        ),
+      ],
     );
   }
 
