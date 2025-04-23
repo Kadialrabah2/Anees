238718272809697904c101a195519960eb6c551c
sana <ssalwaled@gmail.com>
2025-04-23 07:48:38 +0300

    add two language support but not complete

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,26 @@ import 'home.dart';
 import 'describe_feeling.dart';
 import 'chat/talk_to_me.dart';
 import 'progress_tracker.dart'; 
-
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
 }
 
+
+
 class MyApp extends StatelessWidget {
+  
+    Locale _locale = const Locale('ar');
+
+  void _changeLanguage(Locale locale) {
+    setState(() {
+      _locale = locale;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp( 
@@ -29,4 +42,50 @@ class MyApp extends StatelessWidget {
       },
     );
   }
+}
+
+class AppLocalizations {
+  final Locale locale;
+
+  AppLocalizations(this.locale);
+
+  static AppLocalizations of(BuildContext context) {
+    return Localizations.of<AppLocalizations>(context, AppLocalizations)!;
+  }
+
+  late Map<String, String> _localizedStrings;
+
+  Future<void> load() async {
+    String jsonString = await rootBundle
+        .loadString('assets/translation/${locale.languageCode}.json');
+    Map<String, dynamic> jsonMap = json.decode(jsonString);
+
+    _localizedStrings =
+        jsonMap.map((key, value) => MapEntry(key, value.toString()));
+  }
+
+  String translate(String key) {
+    return _localizedStrings[key] ?? key;
+  }
+
+  static const LocalizationsDelegate<AppLocalizations> delegate =
+      _AppLocalizationsDelegate();
+}
+
+class _AppLocalizationsDelegate
+    extends LocalizationsDelegate<AppLocalizations> {
+  const _AppLocalizationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) => ['en', 'ar'].contains(locale.languageCode);
+
+  @override
+  Future<AppLocalizations> load(Locale locale) async {
+    AppLocalizations localizations = AppLocalizations(locale);
+    await localizations.load();
+    return localizations;
+  }
+
+  @override
+  bool shouldReload(covariant LocalizationsDelegate old) => false;
 }
