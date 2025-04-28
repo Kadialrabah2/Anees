b589f2409a49361c545a6a69c541227ff0a455ef
a <a>
2025-04-29 02:40:08 +0300

    home

---
+++ build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods_Runner_vers.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/rawankhalid/Documents/projects/Anees/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
+  /Users/rawankhalid/Documents/projects/Anees/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
---
+++ build/macos/Build/Intermediates.noindex/Pods.build/Debug/flutter_localization.build/Objects-normal/arm64/flutter_localization-dummy.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/rawankhalid/Documents/projects/Anees/build/macos/Build/Intermediates.noindex/Pods.build/Debug/flutter_localization.build/module.modulemap \
+  /Users/rawankhalid/Documents/projects/Anees/macos/Pods/Target\ Support\ Files/flutter_localization/flutter_localization-dummy.m \
+  /Users/rawankhalid/Documents/projects/Anees/macos/Pods/Target\ Support\ Files/flutter_localization/flutter_localization-prefix.pch \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/Cocoa.framework/Modules/module.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/Foundation.framework/Modules/module.modulemap
